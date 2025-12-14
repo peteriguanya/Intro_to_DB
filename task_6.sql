7e48a8c56d08e2efddf763f6ee3c282a808decfa
peteriguanya <iguanyapeter7@gmail.com>
2025-12-14 20:22:48 +0300

    Add task_6.sql: insert multiple customers into customer table

--- task_6.sql
+++ task_6.sql
@@ -1,5 +1,6 @@
-INSERT INTO CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
+-- Insert multiple customers into customer table
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES 
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
